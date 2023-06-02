a790ed0f062df7c646d7c7929e8df3e396249928
RC <rckashyapcpe@gmail.com>
2023-06-02 13:24:45 +0800

    first commit

---
+++ app/src/main/java/com/example/intentactivity/WelcomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.intentactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.intentactivity.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWelcomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
